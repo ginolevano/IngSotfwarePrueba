3b24e839ffb3825142a9b1e19e59969c886e31d9
Gino Levano Carh <gmlevanoc@escuelaescena.com>
2025-10-09 19:30:05 +0200

    dependency

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class Main {
+    
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("HIII!! ESTAMOS HACIENDO TESTING!!");
+        log.info("STARTING ORDER MANAGMENT SYSTEM.....");
+
     }
 }
